ea37b26b622de5435290d4436b0df7b2235d3985
yehor.romaniuk <krasheg@gmail.com>
2024-10-24 12:18:43 +0300

    added migrations, waiting script for db

---
+++ posts_app/utils.py
@@ -0,0 +1,13 @@
+def build_comment_tree(comments):
+    comments = [item.to_json() for item in comments]
+    # Create a dictionary to store children of each comment
+    comment_dict = {comment["id"]: comment for comment in comments}
+    tree_comments = []
+
+    for comment in comments:
+        parent_id = comment.pop("parent", None)
+        if parent_id is None:
+            tree_comments.append(comment)
+        else:
+            comment_dict[parent_id]['children'].append(comment)
+    return tree_comments
